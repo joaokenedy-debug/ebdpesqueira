0fe2fbcab40ef442a6e1f8e6d4f64d8771c81f8d
joaokenedy-debug <joao.kenedy@pbavante.com.br>
2025-10-03 12:15:00 -0300

    __init__.py

--- ebd/__init__.py
+++ ebd/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for,  send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -10,9 +10,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "193c5a16"
 app.config["UPLOAD_FOLDER"] = "static/pedidos"
 database = SQLAlchemy(app)
+migrate = Migrate(app, database)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "home"
 
 
 from ebd import routes
+

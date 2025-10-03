b43d643253eed7d2a2c59205e4640c9ac5138416
joaokenedy-debug <joao.kenedy@pbavante.com.br>
2025-10-03 11:27:35 -0300

    main.py
    ajuste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from ebd import app
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Render usa $PORT, local usa 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=8080)
+
 

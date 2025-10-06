c141b0e7e08e6f7e14941debc9eae319f8e0ed7f
joaokenedy-debug <joao.kenedy@pbavante.com.br>
2025-10-06 17:40:29 -0300

    __init__.py

--- ebd/__init__.py
+++ ebd/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for,  send_file
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
@@ -10,13 +10,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = "193c5a16"
 app.config["UPLOAD_FOLDER"] = "static/pedidos"
+
 database = SQLAlchemy(app)
 migrate = Migrate(app, database)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "home"
 
+# === Cria as tabelas se não existirem ===
+with app.app_context():
+    database.create_all()
+    print("Tabelas criadas (se ainda não existiam)")
 
+# Importa as rotas após inicializar o banco
 from ebd import routes
 
 

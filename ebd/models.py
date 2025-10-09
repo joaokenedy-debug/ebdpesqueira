c6cfd3df3788c2e1a71aae661414b8c89d6d7278
joaokenedy-debug <joao.kenedy@pbavante.com.br>
2025-10-09 12:51:24 -0300

    Update models.py

--- ebd/models.py
+++ ebd/models.py
@@ -15,4 +15,23 @@ class Usuario(database.Model, UserMixin):
     senha = database.Column(database.String, nullable=False)
     is_admin = database.Column(database.Boolean, default=False) 
 
+class Pedido(database.Model):
+    __tablename__ = "pedido"
+    id = database.Column(database.Integer, primary_key=True)
+    id_usuario = database.Column(database.Integer, database.ForeignKey("usuario.id"), nullable=False)
+    congregacao = database.Column(database.String, nullable=False)
+    data = database.Column(database.DateTime, default=datetime.utcnow)
+    total = database.Column(database.Float, nullable=False)
+
+    itens = database.relationship("ItemPedido", backref="pedido", cascade="all, delete-orphan", lazy=True)
+
+class ItemPedido(database.Model):
+    __tablename__ = "item_pedido"
+    id = database.Column(database.Integer, primary_key=True)
+    id_pedido = database.Column(database.Integer, database.ForeignKey("pedido.id"), nullable=False)
+    produto = database.Column(database.String, nullable=False)
+    codigo = database.Column(database.Integer, nullable=False)
+    quantidade = database.Column(database.Integer, nullable=False)
+    preco_unitario = database.Column(database.Float, nullable=False)
+    subtotal = database.Column(database.Float, nullable=False)
 
